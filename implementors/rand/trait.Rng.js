83418d39233683acd56e425ddb2949f6c436f09f
Simonas Kazlauskas <git@kazlauskas.me>
2014-12-20 12:02:48 +0200

    Add documentation for 0.0.2

---
+++ implementors/rand/trait.Rng.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rdrand'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='rdrand/struct.RdRand.html' title='rdrand::RdRand'>RdRand</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['rdrand'] = {"items":[[0,"","rdrand","An implementation of random number generator based on `rdrand` instruction."],[1,"RdRand","",""],[2,"Error","",""],[12,"UnsupportedProcessor","","The processor does not support the `rdrand` instruction.",0],[10,"new","","Build a generator object. The function will only succeed if `rdrand` instruction can be\nsuccessfully used.",1],[10,"next_u16","","Generate a u16 value.",1],[10,"next_u32","","",1],[10,"next_u64","","",1]],"paths":[[2,"Error"],[1,"RdRand"]]};
+initSearch(searchIndex);
