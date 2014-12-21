f9ccf0620d177ce214c646e6d5c9e58c52a83a31
Simonas Kazlauskas <git@kazlauskas.me>
2014-12-21 23:42:17 +0200

    Update docs for 0.0.4

--- implementors/rand/trait.Rng.js
+++ implementors/rand/trait.Rng.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rdrand'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='rdrand/struct.RdRand.html' title='rdrand::RdRand'>RdRand</a>",];
+implementors['rdrand'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='rdrand/struct.RdRand.html' title='rdrand::RdRand'>RdRand</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='rdrand/struct.RdSeed.html' title='rdrand::RdSeed'>RdSeed</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
