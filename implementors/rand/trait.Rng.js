becb4be7be9b448a6454705f7010b1dfa8e08b8c
Simonas Kazlauskas <git@kazlauskas.me>
2015-11-07 20:54:59 +0200

    Update docs

--- implementors/rand/trait.Rng.js
+++ implementors/rand/trait.Rng.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
-implementors['rand'] = [];
-implementors['rdrand'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='rdrand/struct.RdRand.html' title='rdrand::RdRand'>RdRand</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='rdrand/struct.RdSeed.html' title='rdrand::RdSeed'>RdSeed</a>",];
+implementors['rand'] = [];implementors['rdrand'] = ["impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='rdrand/struct.RdRand.html' title='rdrand::RdRand'>RdRand</a>","impl <a class='trait' href='rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='rdrand/struct.RdSeed.html' title='rdrand::RdSeed'>RdSeed</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
